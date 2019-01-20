a204e9fde8123b989400bdd2f642844c9b676e0c
Jacob Dorais <jacobdorais25@hotmail.com>
2019-01-19 20:53:46 -0500

    ajout de get signalisation

--- server/server.rb
+++ server/server.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/cross_origin'
 require 'json'
+require 'net/http'
 
 set(:port, 3000)
 
@@ -15,6 +16,11 @@ end
 get('/') do
   blah = {h: "hello", b: "hello"}
   return JSON.generate(blah)
+end
+
+get('/signalisation') do
+  uri = URI('http://donnees.ville.montreal.qc.ca/dataset/8ac6dd33-b0d3-4eab-a334-5a6283eb7940/resource/52cecff0-2644-4258-a2d1-0c4b3b116117/download/signalisation.json')
+  JSON.parse(Net::HTTP.get(uri))
 end
 
 options "*" do
