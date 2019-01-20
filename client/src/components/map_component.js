e2375f5607655fae7d7626a3de5fa3a816945e0e
Billy <billy.bouchard03@gmail.com>
2019-01-19 20:54:41 -0500

    fix maps component

--- client/src/components/map_component.js
+++ client/src/components/map_component.js
@@ -4,30 +4,30 @@ import MAP_KEY from '../keys'
 import './map_component.scss';
 
 export class Map_Component extends Component {
-  static defaultProps = {
-    center: {
-      lat: 45,
-      lng: -75
-    },
-    zoom: 11
-  };
 
-  constructor(props){
-    super(props);
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (xhttp.readyState === 4 && xhttp.status === 200) {
-         console.log(xhttp.responseText);
-      }
-    };
-    xhttp.open("GET", "http://donnees.ville.montreal.qc.ca/dataset/8ac6dd33-b0d3-4eab-a334-5a6283eb7940/resource/52cecff0-2644-4258-a2d1-0c4b3b116117/download/signalisation.json", true);
-    xhttp.send();
+  constructor(props) {
+    super(props)
+    this.state = {}
+    this.state.center = { lat: 45, lng: -75 }
+    this.state.zoom = 11
+    this.state.map = undefined
+    this.state.maps = undefined
+  }
+
+  on_load_map(map, maps) {
+    this.state.map = map
+    this.state.maps = maps
   }
 
   render() {
     return (
       <div class="map-shower">
-        <GoogleMapReact bootstrapURLKeys={{ key: MAP_KEY }} defaultCenter={this.props.center} defaultZoom={this.props.zoom}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: MAP_KEY }}
+          defaultCenter={this.state.center}
+          defaultZoom={this.state.zoom}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={({ map, maps }) => this.on_load_map(map, maps)}>
           <div lat={45} lng={-75} text="My Marker"></div>
         </GoogleMapReact>
       </div>
