3efb20f5afb83797c58fc2d46abd0bd33c741e27
FX <francois-xavier.legault@polymtl.ca>
2019-01-20 00:12:54 -0500

    what am I doing

--- client/src/components/map_component.js
+++ client/src/components/map_component.js
@@ -12,11 +12,27 @@ export class Map_Component extends Component {
     this.state.zoom = 11
     this.state.map = undefined
     this.state.maps = undefined
+    this.state.paths = [
+        {lat: 45.5760038372319, lng: -73.5668386028432},
+        {lat: 45.5760132973046, lng: -73.5668644965631},
+        {lat: 45.5441418663109, lng: -73.5676036744915},
+        {lat: 45.5440905103725, lng: -73.5676379070403}
+      ]
   }
 
   on_load_map(map, maps) {
     this.state.map = map
     this.state.maps = maps
+    this.draw_poly_lines(this.state.paths[0],this.state.paths[1]);
+    this.draw_poly_lines(this.state.paths[2],this.state.paths[3]);
+  }
+
+  draw_poly_lines(point1, point2){
+    var b = new this.state.maps.Polyline({
+      path: [point1, point2]
+    });
+
+    b.setMap(this.state.map);
   }
 
   render() {
